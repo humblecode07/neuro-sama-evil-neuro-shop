a403ec1c19e757b01e22ff52da488095087200e6
humblecode07 <calderonmiko011@gmail.com>
2026-07-20 14:29:50 +0800

    feat: add product and character data for Neuro-sama and Evil Neuro plushies, including images

---
+++ merch/data.ts
@@ -0,0 +1,134 @@
+const characters = [
+   {
+      name: "Neuro-sama",
+      slug: "neuro-sama"
+   },
+   {
+      name: "Evil Neuro",
+      slug: "evil-neuro"
+   }
+];
+
+const categories = [
+   {
+      name: "Plush",
+      slug: "plush",
+   },
+   {
+      name: "Keychain",
+      slug: "keychain",
+   },
+   {
+      name: "Tees",
+      slug: "tees",
+   },
+   {
+      name: "Hoodie",
+      slug: "hoodie",
+   }
+]
+
+const products = [
+   {
+      name: "Neuro-sama Duality Plush",
+      slug: "neuro-sama-duality-plush",
+      description: "She so qte, an adorable little gremlin plushie wearing her new t-shirt she designed!",
+      character: "neuro-sama",
+      category: "plush",
+      price: 2999,
+      stock: 100,
+      images: [
+         {
+            image_url: "/neuro-sama-duality-plush-fumo_1.jpg",
+            display_order: 0
+         },
+         {
+            image_url: "/neuro-sama-duality-plush-fumo_2.jpg",
+            display_order: 1
+         },
+         {
+            image_url: "/neuro-sama-duality-plush-fumo_3.jpg",
+            display_order: 2
+         },
+         {
+            image_url: "/neuro-sama-duality-plush-fumo_4.jpg",
+            display_order: 3
+         },
+         {
+            image_url: "/neuro-sama-duality-plush-fumo_5.jpg",
+            display_order: 4
+         },
+         {
+            image_url: "/neuro-sama-duality-plush-fumo_6.jpg",
+            display_order: 5
+         },
+         {
+            image_url: "/neuro-sama-duality-plush-fumo_7.jpg",
+            display_order: 6
+         },
+         {
+            image_url: "/neuro-sama-duality-plush-fumo_8.jpg",
+            display_order: 7
+         },
+         {
+            image_url: "/neuro-sama-duality-plush-fumo_9.jpg",
+            display_order: 8
+         },
+         {
+            image_url: "/neuro-sama-duality-plush-fumo_10.jpg",
+            display_order: 9
+         },
+      ]
+   },
+   {
+      name: "Evil Neuro Duality Plush",
+      slug: "evil-neuro-duality-plush",
+      description: "She so qte, an adorable little gremlin plushie wearing her new t-shirt she designed!",
+      character: "evil-neuro",
+      category: "plush",
+      price: 2999,
+      stock: 100,
+      images: [
+         {
+            image_url: "/evil-neuro-duality-plush-fumo_1.jpg",
+            display_order: 0
+         },
+         {
+            image_url: "/evil-neuro-duality-plush-fumo_2.jpg",
+            display_order: 1
+         },
+         {
+            image_url: "/evil-neuro-duality-plush-fumo_3.jpg",
+            display_order: 2
+         },
+         {
+            image_url: "/evil-neuro-duality-plush-fumo_4.jpg",
+            display_order: 3
+         },
+         {
+            image_url: "/evil-neuro-duality-plush-fumo_5.jpg",
+            display_order: 4
+         },
+         {
+            image_url: "/evil-neuro-duality-plush-fumo_6.jpg",
+            display_order: 5
+         },
+         {
+            image_url: "/evil-neuro-duality-plush-fumo_7.jpg",
+            display_order: 6
+         },
+         {
+            image_url: "/evil-neuro-duality-plush-fumo_8.jpg",
+            display_order: 7
+         },
+         {
+            image_url: "/evil-neuro-duality-plush-fumo_9.jpg",
+            display_order: 8
+         },
+         {
+            image_url: "/evil-neuro-duality-plush-fumo_10.jpg",
+            display_order: 9
+         },
+      ]
+   },
+]
