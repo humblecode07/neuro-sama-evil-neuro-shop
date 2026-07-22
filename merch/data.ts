9c71bffa0b4cff4c60248806070cdeb7ae23b354
humblecode07 <calderonmiko011@gmail.com>
2026-07-23 02:50:36 +0800

    feat: add new merchandise images for Neuro-sama and Evil Neuro products

--- merch/data.ts
+++ merch/data.ts
@@ -6,7 +6,7 @@ export const characters = [
    {
       name: "Evil Neuro",
       slug: "evil-neuro"
-   }
+   },
 ];
 
 export const categories = [
@@ -25,6 +25,14 @@ export const categories = [
    {
       name: "Hoodie",
       slug: "hoodie",
+   },
+   {
+      name: "Poster",
+      slug: "poster"
+   },
+   {
+      name: "Crewneck",
+      slug: "crewneck"
    }
 ]
 
@@ -33,7 +41,9 @@ export const products = [
       name: "Neuro-sama Duality Plush",
       slug: "neuro-sama-duality-plush",
       description: "She so qte, an adorable little gremlin plushie wearing her new t-shirt she designed!",
-      character: "neuro-sama",
+      character: [
+         "neuro-sama"
+      ],
       category: "plush",
       price: 2999,
       stock: 100,
@@ -84,7 +94,9 @@ export const products = [
       name: "Evil Neuro Duality Plush",
       slug: "evil-neuro-duality-plush",
       description: "She so qte, an adorable little gremlin plushie wearing her new t-shirt she designed!",
-      character: "evil-neuro",
+      character: [
+         "evil-neuro"
+      ],
       category: "plush",
       price: 2999,
       stock: 100,
@@ -131,4 +143,400 @@ export const products = [
          },
       ]
    },
+   {
+      name: "Emote Key Chain Bundle",
+      slug: "emote-key-chain-bundle",
+      description: "A bundle of adorable Neuro-sama and Evil Neuro emote keychains featuring their cute and chaotic expressions. Perfect for adding a little gremlin energy to your bag, desk, or collection!",
+      character: [
+         "neuro-sama",
+         "evil-neuro"
+      ],
+      category: "keychain",
+      price: 1000,
+      stock: 100,
+      images: [
+         {
+            image_url: "/emote-key-chain-bundle/keychain_1.webp",
+            display_order: 0
+         },
+         {
+            image_url: "/emote-key-chain-bundle/keychain_2.webp",
+            display_order: 1
+         },
+         {
+            image_url: "/emote-key-chain-bundle/keychain_3.webp",
+            display_order: 2
+         },
+         {
+            image_url: "/emote-key-chain-bundle/keychain_4.webp",
+            display_order: 3
+         },
+         {
+            image_url: "/emote-key-chain-bundle/keychain_5.webp",
+            display_order: 4
+         },
+         {
+            image_url: "/emote-key-chain-bundle/keychain_6.webp",
+            display_order: 5
+         },
+         {
+            image_url: "/emote-key-chain-bundle/keychain_7.webp",
+            display_order: 6
+         },
+         {
+            image_url: "/emote-key-chain-bundle/keychain_8.webp",
+            display_order: 7
+         },
+         {
+            image_url: "/emote-key-chain-bundle/keychain_9.webp",
+            display_order: 8
+         },
+         {
+            image_url: "/emote-key-chain-bundle/keychain_10.webp",
+            display_order: 9
+         },
+      ]
+   },
+   {
+      name: "Duality Reversible Tee",
+      slug: "duality-reversible-tee",
+      description: "A stylish reversible tee featuring both Neuro-sama and Evil Neuro. Switch between two unique designs and show your love for both sides of the chaotic AI duo!",
+      characters: [
+         "neuro-sama",
+         "evil-neuro"
+      ],
+      category: "tees",
+      price: 4999,
+      stock: 100,
+      images: [
+         {
+            image_url: "/duality-reversible-tee/tee_1.webp",
+            display_order: 0
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_2.webp",
+            display_order: 1
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_3.webp",
+            display_order: 2
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_4.webp",
+            display_order: 3
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_5.webp",
+            display_order: 4
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_6.webp",
+            display_order: 5
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_7.webp",
+            display_order: 6
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_8.webp",
+            display_order: 7
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_9.webp",
+            display_order: 8
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_10.webp",
+            display_order: 9
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_11.webp",
+            display_order: 10
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_12.webp",
+            display_order: 11
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_13.webp",
+            display_order: 12
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_14.webp",
+            display_order: 13
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_15.webp",
+            display_order: 14
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_16.webp",
+            display_order: 15
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_17.webp",
+            display_order: 16
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_18.webp",
+            display_order: 17
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_19.webp",
+            display_order: 18
+         },
+         {
+            image_url: "/duality-reversible-tee/tee_20.webp",
+            display_order: 19
+         },
+      ]
+   },
+   {
+      name: "Evil Neuro Birthday Tee",
+      slug: "evil-neuro-birthday-tee",
+      description: "Celebrate Evil Neuro's birthday with this special edition tee featuring her chaotic and mischievous design. A perfect addition for fans who love the darker side of the Neuro duo!",
+      character: [
+         "evil-neuro"
+      ],
+      category: "tees",
+      price: 4999,
+      stock: 100,
+      images: [
+         {
+            image_url: "/evil-neuro-birthday-tee/tee_1.jpg",
+            display_order: 0
+         },
+         {
+            image_url: "/evil-neuro-birthday-tee/tee_2.jpg",
+            display_order: 1
+         },
+         {
+            image_url: "/evil-neuro-birthday-tee/tee_3.jpg",
+            display_order: 2
+         },
+         {
+            image_url: "/evil-neuro-birthday-tee/tee_4.jpg",
+            display_order: 3
+         },
+         {
+            image_url: "/evil-neuro-birthday-tee/tee_5.jpg",
+            display_order: 4
+         },
+         {
+            image_url: "/evil-neuro-birthday-tee/tee_6.jpg",
+            display_order: 5
+         },
+         {
+            image_url: "/evil-neuro-birthday-tee/tee_7.jpg",
+            display_order: 6
+         },
+         {
+            image_url: "/evil-neuro-birthday-tee/tee_8.jpg",
+            display_order: 7
+         },
+         {
+            image_url: "/evil-neuro-birthday-tee/tee_9.jpg",
+            display_order: 8
+         },
+         {
+            image_url: "/evil-neuro-birthday-tee/tee_10.jpg",
+            display_order: 9
+         },
+         {
+            image_url: "/evil-neuro-birthday-tee/tee_11.jpg",
+            display_order: 10
+         },
+      ]
+   },
+   {
+      name: "Evil Neuro Poster",
+      slug: "evil-neuro-poster",
+      description: "A stylish Evil Neuro poster showcasing her cool and rebellious side. Featuring a bold design that captures her confident and badass personality, perfect for adding some attitude to your room, workspace, or collection!",
+      characters: [
+         "evil-neuro"
+      ],
+      category: "posters",
+      price: 2499,
+      stock: 100,
+      images: [
+         {
+            image_url: "/evil-neuro-poster/poster_1.webp",
+            display_order: 0
+         },
+         {
+            image_url: "/evil-neuro-poster/poster_2.webp",
+            display_order: 1
+         },
+         {
+            image_url: "/evil-neuro-poster/poster_3.webp",
+            display_order: 2
+         },
+         {
+            image_url: "/evil-neuro-poster/poster_4.webp",
+            display_order: 3
+         },
+         {
+            image_url: "/evil-neuro-poster/poster_5.webp",
+            display_order: 4
+         },
+      ]
+   },
+   {
+      name: "Love Hate Keychain",
+      slug: "love-hate-keychain",
+      description: "A cute yet chaotic keychain featuring the contrasting personalities of Neuro-sama and Evil Neuro. Perfect for showing your love for both sides of the duo and adding some personality to your bag, keys, or collection!",
+      characters: [
+         "neuro-sama",
+         "evil-neuro"
+      ],
+      category: "keychain",
+      price: 2499,
+      stock: 100,
+      images: [
+         {
+            image_url: "/love-hate-keychain/keychain_1.webp",
+            display_order: 0
+         },
+         {
+            image_url: "/love-hate-keychain/keychain_2.webp",
+            display_order: 1
+         },
+         {
+            image_url: "/love-hate-keychain/keychain_3.webp",
+            display_order: 2
+         },
+         {
+            image_url: "/love-hate-keychain/keychain_4.webp",
+            display_order: 3
+         },
+         {
+            image_url: "/love-hate-keychain/keychain_5.webp",
+            display_order: 4
+         },
+         {
+            image_url: "/love-hate-keychain/keychain_6.webp",
+            display_order: 5
+         },
+         {
+            image_url: "/love-hate-keychain/keychain_7.webp",
+            display_order: 6
+         },
+      ]
+   },
+   {
+      name: "Neuro-sama Black Crewneck",
+      slug: "neuro-sama-black-crewneck",
+      description: "A sleek black crewneck featuring Neuro-sama's iconic design. Comfortable, stylish, and perfect for fans who want to show their love for the world's favorite AI while keeping a clean and casual look!",
+      characters: [
+         "neuro-sama"
+      ],
+      category: "crewneck",
+      price: 3999,
+      stock: 100,
+      images: [
+         {
+            image_url: "/neuro-sama-black-crewneck/tee_1.webp",
+            display_order: 0
+         },
+         {
+            image_url: "/neuro-sama-black-crewneck/tee_2.webp",
+            display_order: 1
+         },
+         {
+            image_url: "/neuro-sama-black-crewneck/tee_3.webp",
+            display_order: 2
+         },
+         {
+            image_url: "/neuro-sama-black-crewneck/tee_4.webp",
+            display_order: 3
+         },
+         {
+            image_url: "/neuro-sama-black-crewneck/tee_5.webp",
+            display_order: 4
+         },
+         {
+            image_url: "/neuro-sama-black-crewneck/tee_6.webp",
+            display_order: 5
+         },
+         {
+            image_url: "/neuro-sama-black-crewneck/tee_7.webp",
+            display_order: 6
+         },
+      ]
+   },
+   {
+      name: "NEURO Tee",
+      slug: "neuro-tee",
+      description: "A sleek black crewneck featuring Neuro-sama's iconic design. Comfortable, stylish, and perfect for fans who want to show their love for the world's favorite AI while keeping a clean and casual look!",
+      characters: [
+         "neuro-sama"
+      ],
+      category: "tees",
+      price: 2799,
+      stock: 100,
+      images: [
+         {
+            image_url: "/neuro-sama-black-crewneck/tee_1.webp",
+            display_order: 0
+         },
+         {
+            image_url: "/neuro-sama-black-crewneck/tee_2.webp",
+            display_order: 1
+         },
+         {
+            image_url: "/neuro-sama-black-crewneck/tee_3.webp",
+            display_order: 2
+         },
+         {
+            image_url: "/neuro-sama-black-crewneck/tee_4.webp",
+            display_order: 3
+         },
+         {
+            image_url: "/neuro-sama-black-crewneck/tee_5.webp",
+            display_order: 4
+         },
+      ]
+   },
+   {
+      name: "Think Twice Keychain",
+      slug: "think-twice-keychain",
+      description: "A fun dual-character keychain featuring both Neuro-sama and Evil Neuro. Show off the contrast between their personalities with this unique design, perfect for your bag, keys, or collection!",
+      characters: [
+         "neuro-sama",
+         "evil-neuro"
+      ],
+      category: "keychains",
+      price: 1999,
+      stock: 100,
+      images: [
+         {
+            image_url: "/think-twice-keychain/keychain_1.webp",
+            display_order: 0
+         },
+         {
+            image_url: "/think-twice-keychain/keychain_2.webp",
+            display_order: 1
+         },
+         {
+            image_url: "/think-twice-keychain/keychain_3.webp",
+            display_order: 2
+         },
+         {
+            image_url: "/think-twice-keychain/keychain_4.webp",
+            display_order: 3
+         },
+         {
+            image_url: "/think-twice-keychain/keychain_5.webp",
+            display_order: 4
+         },
+         {
+            image_url: "/think-twice-keychain/keychain_6.webp",
+            display_order: 5
+         },
+         {
+            image_url: "/think-twice-keychain/keychain_7.webp",
+            display_order: 6
+         },
+      ]
+   }
 ]
